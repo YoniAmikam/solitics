958c0b8d507b2fc5a4bdebb6b470aac06ce5bec8
yoniamikam <yoni.amikam@linearb.io>
2024-09-04 22:34:20 +0300

    add type to vars

--- terraform/environment/dev-01/vars.tf
+++ terraform/environment/dev-01/vars.tf
@@ -1,19 +1,34 @@
 variable "env_name" {
+  type    = string
   default = "dev-01"
 }
 
 variable "aws_account_id" {
+  type    = string
   default = "096432477737"
 }
 
 variable "nginx_replicas" {
+  type    = number
   default = 3
 }
 
 variable "region" {
+  type    = string
   default = "eu-west-1"
 }
 
 variable "alb_1_name" {
+  type    = string
   default = "alb-1"
+}
+
+variable "ssl_policy" {
+  type    = string
+  default = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "sintersoli.com"
 }
